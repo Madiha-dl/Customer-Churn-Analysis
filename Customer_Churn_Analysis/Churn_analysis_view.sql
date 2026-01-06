7c67fb04f19f64617832e7c9949f8fb410df8379
Madiha Refai <refaimadiha007@gmail.com>
2026-01-06 11:43:31 +0400

    Delete Customer_Churn_Analysis directory

--- Customer_Churn_Analysis/Churn_analysis_view.sql
+++
@@ -1,5 +0,0 @@
-CREATE VIEW vw_ChurnData AS
-	SELECT * FROM customer_data WHERE Customer_Status IN ('Churned', 'Stayed');
-
-CREATE VIEW vw_JoinData AS
-	SELECT * FROM customer_data WHERE Customer_Status = 'Joined';
